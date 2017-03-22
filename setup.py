9d8b9e9b04f492cd8d5ed524598294f5986f0168
Philippe Trempe <philippe-2.trempe@polymtl.ca>
2017-03-22 16:42:38 -0400

    Added project setup and configuration files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="conviz",
+    packages=["conviz"],
+    version="0.1",
+    description="A convolutional neural layer visualization library",
+    author="Philippe Trempe",
+    author_email="ph.trempe@gmail.com",
+    url="https://github.com/PhTrempe/conviz",
+    download_url="https://github.com/phtrempe/conviz/archive/0.1.tar.gz",
+    keywords=["convolutional", "neural", "layer", "visualization"],
+    classifiers=[],
+)
