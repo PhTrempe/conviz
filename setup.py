daebf9b283db6bab858578404686706b186dd3dd
Philippe Trempe <philippe-2.trempe@polymtl.ca>
2017-04-04 17:01:31 -0400

    Updated project version to 0.3.

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+VERSION = "0.3"
+
 setup(
     name="conviz",
-    version="0.2",
+    version=VERSION,
     description="A convolutional neural layer visualization library",
     author="Philippe Trempe",
     author_email="ph.trempe@gmail.com",
     license="MIT",
     url="https://github.com/PhTrempe/conviz",
-    download_url="https://github.com/phtrempe/conviz/archive/0.1.tar.gz",
-    keywords=["convolutional", "neural", "layer", "visualization"],
+    download_url="https://github.com/phtrempe/conviz/archive/{}.tar.gz".format(
+        VERSION),
+    keywords=["convolutional", "neural", "network", "layer", "visualization"],
     classifiers=[],
     packages=find_packages(),
     requires=["numpy", "scipy", "pillow", "keras"]
